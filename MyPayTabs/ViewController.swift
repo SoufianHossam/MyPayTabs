8036c0e328c9ca6082393a0863aea4864b845ccf
Soufian Hossam <soufian.el.hossary@gmail.com>
2020-06-17 21:08:38 +0200

    Initial Commit

---
+++ MyPayTabs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPayTabs
+//
+//  Created by Soufian Hossam on 6/17/20.
+//  Copyright © 2020 Soufian Hossam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
